97504cdecadc8d485e56a9547dedccf66f12131e
takeuchi <g.takeuchi@gmail.com>
2017-04-05 15:59:36 -0300

    [DEVOPS-694] renaming field so they match then names returned by jira api

--- src/main/java/br/objective/jira/rest/UserData.java
+++ src/main/java/br/objective/jira/rest/UserData.java
@@ -3,15 +3,15 @@ package br.objective.jira.rest;
 import com.atlassian.jira.user.ApplicationUser;
 
 public class UserData {
+	public String displayName;
+	public String emailAddress;
 	public String name;
-	public String email;
-	public String id;
 
 	public static UserData fromJiraUser(ApplicationUser user) {
 		UserData u = new UserData();
-		u.id = user.getName();
-		u.name = user.getDisplayName();
-		u.email= user.getEmailAddress();
+		u.name = user.getName();
+		u.displayName = user.getDisplayName();
+		u.emailAddress= user.getEmailAddress();
 		return u;
 	}
 }
